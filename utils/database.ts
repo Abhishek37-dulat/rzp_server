bc59c46030e035e4a3a3238364079507e40c28ff
abhishek37-dulat <abhishek11906997dulat@gmail.com>
2024-05-05 05:32:22 +0530

    server

--- utils/database.ts
+++ utils/database.ts
@@ -1,8 +1,15 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
-const sequelize = new Sequelize("newdatastorage", "root", "funtolaughAD@123", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME ?? "",
+  process.env.SQL_USERNAME ?? "",
+  process.env.SQL_PASSWORD ?? "",
+  {
+    host: process.env.DB_HOST ?? "",
+    dialect: "mysql",
+  }
+);
 
 export default sequelize;
